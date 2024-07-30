34688bf2942b7fd1f3c0884f4ddc171bd3eb4183
seon022 <seon022@icloud.com>
2024-07-31 00:36:18 +0900

    zerocho 객체

--- 0730/iterator.js
+++ 0730/iterator.js
@@ -1,18 +1,18 @@
-// let arr = [1, 2, 3, 4, 5];
-// let iterator = arr[Symbol.iterator]();
+let arr = [1, 2, 3, 4, 5];
+let iterator = arr[Symbol.iterator]();
 
-// console.log(iterator);
+console.log(iterator);
 
-// let result = iterator.next();
-// console.log(result);
+let result = iterator.next();
+console.log(result);
 
-// while (!result.done) {
-//   console.log(result.value);
-//   result = iterator.next();
-// }
+while (!result.done) {
+  console.log(result.value);
+  result = iterator.next();
+}
 
-// let copy = [...iterator];
-// console.log(copy);
+let copy = [...iterator];
+console.log(copy);
 
 // -------------------------------------------
 // class Sequence {
--- zerocho_practice/object_0730.js
+++ zerocho_practice/object_0730.js
@@ -1,7 +1,14 @@
 // 객체끼리는 비교하면 false 나옴,( 배열도 함수도,)
 //  {} === {}   => false (객체 입력한 순간 새로운 객체가 생성된거라서)
 
-const a = { name: "zerocho" };
+const a = {
+  name: {
+    first: "k",
+    last: "name",
+  },
+  gender: "m",
+};
+console.log(a.name.first);
 // 객체 리터럴
 
 // 변수 안에 넣어놓고 비교해야 true나옴
@@ -19,3 +26,17 @@ console.log(a === array[2]);
 // "객체"인 경우에는 변수에는 주소가 저장되어있고
 
 // 객체가 아닌 경우, 일반값인 경우에는 그 값이 저장되어있다고 볼 수 있음
+
+// 참조 와 복사
+const arrA = { name: "zerocho" };
+const arrB = arrA;
+// 두 변수 모두 같은 객체를 저장하고 있는것임. 이러한 상황일때, arrA와 arrB, 그리고 객체 간에 참조관계가 있다고 표현함.
+arrA.name = "hero";
+console.log(arrB.name);
+
+// 객체가 아닌거.
+let c = 12;
+let d = c;
+c = 0;
+console.log(c);
+console.log(d);
